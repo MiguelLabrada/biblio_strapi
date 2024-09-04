f238d817f3702b42ecb602556daa38aa4193a0bb
MiguelLabrada <miguel.labrada@udc.es>
2024-09-04 17:55:10 +0200

    refactor: improved the execution order of middleware request-renewal instructions

--- src/api/prestamo/middlewares/request-renewal.js
+++ src/api/prestamo/middlewares/request-renewal.js
@@ -5,29 +5,30 @@ module.exports = (config, { strapi }) => {
   return async (ctx, next) => {
     const currentUserId = ctx.state.user.id;
     const currentUserRoleId = ctx.state?.user?.role.id;
-    const requestedLoan = ctx.params?.id;
 
     if (!currentUserId) {
       return ctx.unauthorized("You are not authorized to perform this action.");
     }
-
-    const prestamo = await strapi.service("api::prestamo.prestamo").findOne(requestedLoan, {
-        populate: ["usuario"]
-    });
     
-    sanitizeRequestBody(ctx, currentUserRoleId, currentUserId, prestamo);
+    sanitizeRequestBody(ctx, currentUserRoleId, currentUserId);
 
     await next();
   };
 
-  function sanitizeRequestBody(ctx, currentUserRoleId, currentUserId, prestamo) {
+  async function sanitizeRequestBody(ctx, currentUserRoleId, currentUserId) {
     if(currentUserRoleId == 3){
       const { ejemplar, usuario, ...newData } = ctx.request.body.data;
       ctx.request.body.data = newData;
-    } else if (currentUserRoleId == 6 && Number(currentUserId) == Number(prestamo.usuario.id)) {
-      ctx.request.body.data.renovacion_solicitada = true;
-      const { renovacion_solicitada } = ctx.request.body.data;
-      ctx.request.body.data = { renovacion_solicitada };
+    } else if (currentUserRoleId == 6) {
+      const requestedLoan = ctx.params?.id;
+      const prestamo = await strapi.service("api::prestamo.prestamo").findOne(requestedLoan, {
+        populate: ["usuario"]
+      });
+      if (Number(currentUserId) == Number(prestamo.usuario.id)) {
+        ctx.request.body.data.renovacion_solicitada = true;
+        const { renovacion_solicitada } = ctx.request.body.data;
+        ctx.request.body.data = { renovacion_solicitada };
+      }
     } else {
       return ctx.throw(403, 'You are not allowed to update a loan for this user.');
     }
